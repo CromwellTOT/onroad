737c4a215ba66802b6ca1f1961fff13ec0765a35
Frank Zhang <fzhang@aetvn.com>
2018-05-02 19:58:19 -0400

    pop up a dialog when user is going to delete account

--- public/js/service.js
+++ public/js/service.js
@@ -75,3 +75,22 @@ app.directive('focusOn', function() {
       });
    };
 });
+// /app/directives/ng-confirm-click.js
+app.directive('ngConfirmClick', [
+  function(){
+    return {
+      priority: -1,
+      restrict: 'A',
+      link: function(scope, element, attrs){
+        element.bind('click', function(e){
+          var message = attrs.ngConfirmClick;
+          // confirm() requires jQuery
+          if(message && !confirm(message)){
+            e.stopImmediatePropagation();
+            e.preventDefault();
+          }
+        });
+      }
+    }
+  }
+]);
